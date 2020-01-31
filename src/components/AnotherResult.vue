f7bfec57cdd66bbee9e63e5697b684bfc7a8841b
Rabel Obispo <rabelobispo@hotmail.com>
2020-01-30 23:05:09 -0500

    Setting getters

---
+++ src/components/AnotherResult.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>Counter is: {{ counter }}</p>
+    <p>Number of clicks: {{ clicks }}</p>
+  </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex';
+  
+  export default {
+    computed: mapGetters({
+      counter: 'doubleCounter',
+      clicks: 'stringCounter'
+    })
+  };
+</script>
--- src/store/store.js
+++ src/store/store.js
@@ -6,5 +6,9 @@ Vue.use(Vuex);
 export const store = new Vuex.Store({
   state: {
     counter: 0
+  },
+  getters: {
+    doubleCounter: s => s.counter * 2,
+    stringCounter: s => `${s.counter} clicks!`
   }
 });
